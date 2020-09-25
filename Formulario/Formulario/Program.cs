9af020b3e0568170fcae53c329eb60d4124efc0d
Alexa-20 <71473066+Alexa-20@users.noreply.github.com>
2020-09-25 15:35:56 -0500

    Fornulario

---
+++ Formulario/Formulario/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Formulario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String n, ap, am, nt, c, o;
+            int e;
+            Console.Write("Ingresa los datos que se solicitan a continuacion: ");
+            Console.WriteLine("\nNombre: ");
+            n = Console.ReadLine();
+            Console.WriteLine("\nApellido paterno: ");
+            ap = Console.ReadLine();
+            Console.WriteLine("\nApellido Materno: ");
+            am = Console.ReadLine();
+            Console.WriteLine("\nEdad: ");
+            e = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nNumero Telefonico: ");
+            nt = Console.ReadLine();
+            Console.WriteLine("\nCorreo: ");
+            c = Console.ReadLine();
+            Console.WriteLine("\nOcupacion: ");
+            o = Console.ReadLine();
+
+            if (e > 18)
+            {
+
+            }
+
+        }
+    }
+}
