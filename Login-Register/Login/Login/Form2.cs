323fd23731157e3ee5b80f838377d12f92fd4752
greciarb16 <71858491+greciarb16@users.noreply.github.com>
2020-10-27 12:09:39 -0600

    Parte 2
    Creacion del formulario 2 (Diseño y codigo)

--- Login-Register/Login/Login/Form2.cs
+++ Login-Register/Login/Login/Form2.cs
@@ -1,5 +1,4 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,25 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
-namespace Login
+namespace MYLOGIN
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form2_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void Ingresar()
+        private void Register()
         {
             string connect = "datasource=localhost;port=3306;username=root;password=;database=mylogin";
-            string query = "select * from mylogin where User = '" + Userbox.Text + "' AND Password = '" + Passbox.Text + "'";
+            string query = "INSERT INTO mylogin (`ID`, `User`, `Password`) Values (Null, '" + Userbox.Text + "', '" + Passbox.Text + "')";
             MySqlConnection databaseConnection = new MySqlConnection(connect);
             MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
             commandDatabase.CommandTimeout = 60;
@@ -36,14 +36,9 @@ namespace Login
             {
                 databaseConnection.Open();
                 reader = commandDatabase.ExecuteReader();
-                if (reader.Read())
-                {
-                    MessageBox.Show("Bienvenido al sistema <3");
-                }
-                else
-                {
-                    MessageBox.Show("Usuario o Contraseña incorrecto :c");
-                }
+                MessageBox.Show("Usuario registrado exitosamente <3");
+                databaseConnection.Close();
+
             }
 
             catch (Exception ex)
@@ -51,10 +46,8 @@ namespace Login
                 MessageBox.Show(ex.Message);
             }
         }
-   
 
-
-        private void label2_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
@@ -74,19 +67,10 @@ namespace Login
 
         }
 
-        private void Loginbut_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            Ingresar();
-        }
-
-        private void Registerbut_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
+            Register();
         }
     }
+
 }
